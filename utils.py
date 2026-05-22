97cf9f2953c7ee6025d7808f036b48f0c457ed3b
srudolph25 <s_rudolphs25@stud.hwr-berlin.de>
2026-05-22 12:15:05 +0200

    Return only male passengers

--- utils.py
+++ utils.py
@@ -4,3 +4,6 @@ import pandas as pd
 def load_data() -> pd.DataFrame:
     """Load the Titanic dataset into a pandas DataFrame."""
     return pd.read_csv("data/titanic.csv")
+
+def filter_passengers(df):
+    return df[df["sex"] == "male"]
